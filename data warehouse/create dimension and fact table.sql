8eb58ff2d36ee8889b08afb3387bf6b32125f643
vuhuusy <vuhuusytft1@gmail.com>
2024-03-31 11:55:24 +0700

    add star schema model add script to create dimension and fact table

---
+++ data warehouse/create dimension and fact table.sql
@@ -0,0 +1,87 @@
+DROP TABLE SYVH_DIM_CUSTOMERS;
+DROP TABLE SYVH_DIM_OFFICES;
+DROP TABLE SYVH_DIM_PRODUCTS;
+DROP TABLE SYVH_DIM_EMPLOYEES;
+DROP TABLE SYVH_DIM_DATE;
+DROP TABLE SYVH_FACT_ORDERLINE_TRANSACTION;
+
+CREATE TABLE SYVH_DIM_CUSTOMERS (
+  customerKey INT PRIMARY KEY,
+  customerNumber INT,
+  customerName VARCHAR2(50),
+  contactLastName VARCHAR2(50),
+  contactFirstName VARCHAR2(50),
+  phone VARCHAR2(50),
+  addressLine1 VARCHAR2(50),
+  addressLine2 VARCHAR2(50),
+  city VARCHAR2(50),
+  state VARCHAR2(50),
+  postalCode VARCHAR2(15),
+  country VARCHAR2(50),
+  creditLimit DECIMAL(10,2)
+);
+
+CREATE TABLE SYVH_DIM_OFFICES (
+  officeKey INT PRIMARY KEY,
+  officeCode VARCHAR2(10),
+  city VARCHAR2(50),
+  phone VARCHAR2(50),
+  addressLine1 VARCHAR2(50),
+  addressLine2 VARCHAR2(50),
+  state VARCHAR2(50),
+  country VARCHAR2(50),
+  postalCode VARCHAR2(15),
+  territory VARCHAR2(10)
+);
+
+CREATE TABLE SYVH_DIM_PRODUCTS (
+  productKey INT PRIMARY KEY,
+  productCode VARCHAR2(15),
+  productName VARCHAR2(70),
+  productLine VARCHAR2(50),
+  productScale VARCHAR2(10),
+  productVendor VARCHAR2(50),
+  productDescription VARCHAR2(1000),
+  MSRP DECIMAL(10,2),
+  productLineDescription VARCHAR2(4000)
+);
+
+CREATE TABLE SYVH_DIM_EMPLOYEES (
+  employeeKey INT PRIMARY KEY,
+  employeeNumber INT,
+  lastName VARCHAR2(50),
+  firstName VARCHAR2(50),
+  extension VARCHAR2(10),
+  email VARCHAR2(100),
+  officeCode VARCHAR2(10),
+  reportsTo INT,
+  jobTitle VARCHAR2(50)
+);
+
+CREATE TABLE SYVH_DIM_DATE (
+  dateKey VARCHAR2(8) PRIMARY KEY,
+  fulldate DATE,
+  dayOfMonth INT,
+  monthOfYear INT,
+  quarter INT,
+  year INT,
+  dayOfWeek VARCHAR2(20),
+  weekdayIndicator VARCHAR2(20)
+);
+
+CREATE TABLE SYVH_FACT_ORDERLINE_TRANSACTION (
+  orderNumber INT,
+  orderLineNumber INT,
+  orderDateKey VARCHAR2(8),
+  requiredDateKey VARCHAR2(8),
+  shippedDateKey VARCHAR2(8),
+  customerKey INT,
+  saleRepKey INT,
+  officeKey INT,
+  productKey INT,
+  quantityOrdered INT,
+  priceEach DECIMAL(10,2),
+  buyPrice DECIMAL(10,2),
+  cost DECIMAL(20,4),
+  revenue DECIMAL(20,4)
+);

fa3b5f0a92c2d55a7ff309e9f34ac6e3e5c3ac64
vuhuusy <vuhuusytft1@gmail.com>
2024-04-04 14:55:34 +0700

    create data mart tables command

---
+++ data mart/createa_datamart_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE SYVH_DMART_PRODUCT (
+    productKey INT,
+    month NUMBER,
+    quantitySold INT,
+    cost DECIMAL(38,8),
+    revenue DECIMAL(38,8),
+    profit DECIMAL(38,8)
+);
+
+
+

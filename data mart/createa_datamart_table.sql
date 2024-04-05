a2f20b57b4661877dc39f231bdd89efe0b5a61bb
vuhuusy <vuhuusytft1@gmail.com>
2024-04-05 23:53:21 +0700

    update files

--- data mart/createa_datamart_table.sql
+++ data mart/createa_datamart_table.sql
@@ -1,6 +1,42 @@
+DROP TABLE SYVH_DMART_PRODUCT;
+DROP TABLE SYVH_DMART_CUSTOMER;
+DROP TABLE SYVH_DMART_EMPLOYEE;
+DROP TABLE SYVH_DMART_OFFICE;
+
 CREATE TABLE SYVH_DMART_PRODUCT (
     productKey INT,
-    month NUMBER,
+    productName VARCHAR2(70),
+    month VARCHAR(6),
+    quantitySold INT,
+    cost DECIMAL(38,8),
+    revenue DECIMAL(38,8),
+    profit DECIMAL(38,8)
+);
+
+CREATE TABLE SYVH_DMART_CUSTOMER (
+    customerKey INT,
+    productKey INT,
+    month VARCHAR(6),
+    quantitySold INT,
+    cost DECIMAL(38,8),
+    revenue DECIMAL(38,8),
+    profit DECIMAL(38,8)
+);
+
+CREATE TABLE SYVH_DMART_EMPLOYEE (
+    employeeKey INT,
+    productKey INT,
+    month VARCHAR(6),
+    quantitySold INT,
+    cost DECIMAL(38,8),
+    revenue DECIMAL(38,8),
+    profit DECIMAL(38,8)
+);
+
+CREATE TABLE SYVH_DMART_OFFICE (
+    officeKey INT,
+    productKey INT,
+    month VARCHAR(6),
     quantitySold INT,
     cost DECIMAL(38,8),
     revenue DECIMAL(38,8),
